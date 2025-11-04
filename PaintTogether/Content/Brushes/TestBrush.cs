e22f82c1781db8b27757357d0ec38adb44826261
Glasta <Glasta326@gmail.com>
2025-11-04 19:29:17 +0000

    Maybe it signed in now?

--- PaintTogether/Content/Brushes/TestBrush.cs
+++ PaintTogether/Content/Brushes/TestBrush.cs
@@ -27,7 +27,7 @@ namespace PaintTogether.Content.Brushes
         Texture2D t;
 
 
-        protected override Color? BrushDraw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, Point offset)
+        protected override Color? BrushDraw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
         {
             return Color.Aqua;
         }
